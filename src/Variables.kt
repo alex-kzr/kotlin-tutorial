8f4905747440f3b8c941686d9ded39896b413022
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-21 00:32:06 +1000

    Add variablese sample

---
+++ src/Variables.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val string1: String = "Hello"
+    // string1 = "Goodbye" - error
+
+    var string2: String = "Hello"
+    string2 = "Goodbye"
+
+    var string3 = "Hello" // auto use String type
+
+    var int1 = 3;
+    var int2 = 15;
+
+    println("Value of test2 = $string2")
+    println("Value of test3 = $string3")
+    println("Sum is ${2 + 2}")
+    println("Sum is ${int2 - int1}")
+}
